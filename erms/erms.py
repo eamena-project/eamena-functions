a1cb32978d35011d9503831416121ab27899ac85
zoometh <thomashuet7@gmail.com>
2023-11-01 17:18:41 +0000

    radio button

--- erms/erms.py
+++ erms/erms.py
@@ -168,30 +168,34 @@ def filter_hp_by_gs(hps, selected_hp):
 	:param selected_hp: a list of HPs IDs
 
 	:return: Update a copy of the HP list filtered on a given GS
+
+	:Example: 
+	>> filter_hp_by_gs(hps, selected_hp) # shows the button
+	>> filtered_hp_gs # print the list
 	"""
 	# global filtered_hp_gs
 	l_GridIDs = []
-	for i in range(len(selected_hp)):
-		l_GridIDs.append(hps['features'][i]['properties']['Grid ID'])
-	l_GridIDs = list(set(l_GridIDs))
-	l_GridIDs.sort()
-	radio_button_1 = widgets.RadioButtons(
-		options = l_GridIDs,
-		description = 'Select a Grid Square:'
-	)
-	output = widgets.Output()
+    for i in range(len(selected_hp)):
+        l_GridIDs.append(hps['features'][i]['properties']['Grid ID'])
+    l_GridIDs = list(set(l_GridIDs))
+    l_GridIDs.sort()
+    radio_button_1 = widgets.RadioButtons(
+        options=l_GridIDs,
+        description='Select a Grid Square:'
+    )
+    output = widgets.Output()
 
-	def on_radio_button_change(change):
-		selected_value = change.new
-		global filtered_hp_gs  # Use global to access the global filtered_hp_gs
-		with output:
-			output.clear_output()
-			filtered_hp_gs = filter_dataframe(hps, selected_hp, selected_value)
-			# display(filtered_hp_gs)
-			print(filtered_hp_gs, end="")
-	
-	radio_button_1.observe(on_radio_button_change, names='value')
-	display(radio_button_1, output)
+    def on_radio_button_change(change):
+        selected_value = change.new
+        global filtered_hp_gs
+        with output:
+            output.clear_output()
+            filtered_hp_gs = filter_dataframe(hps, selected_hp, selected_value)
+            # Print or display the result
+            print(filtered_hp_gs, end="")
+
+    radio_button_1.observe(on_radio_button_change, names='value')
+    display(radio_button_1, output)
 
 def hps_subset_by_gs(hps, filtered_hp_gs):
 	"""
