2abc31ca785e3274bf5ba1bf821c3c3db6b0770e
zoometh <thomashuet7@gmail.com>
2023-11-17 12:15:13 +0000

    zenodo

--- zenodo/zenodo.py
+++ zenodo/zenodo.py
@@ -66,3 +66,38 @@ def zenodo_dates(data = None, fields = ["Assessment Activity Date"]):
     DATES = [{'type': 'created', 'start': min_date_str, 'end': max_date_str}]
     return DATES
 
+def zenodo_related_identifiers(site = 'https://zenodo.org/oai2d', set = 'user-eamena', metadataPrefix = 'oai_dc', reference_data_list = "https://raw.githubusercontent.com/eamena-project/eamena-arches-dev/main/data/lod/zenodo/reference_data_list.tsv"):
+    """
+    Parse the 'EAMENA database' community in Zenodo ('user-eamena') to check if there are already uploaded datasets. Handle differently the refrence data (collections, RMs, ...) and the datasets, or business data. The former are 'isDescribedBy' related identifiers, whereas the latter are 'isContinuedBy' related resources.
+
+    :param reference_data_list: the list of reference data already existing in the 'eamena' Zenodo community. These objects will not be added as 'isContinuedBy' in the metadata key 'related_identifiers' but as 'isDescribedBy'
+    """
+    from sickle import Sickle
+
+    # reference_data_list = "https://raw.githubusercontent.com/eamena-project/eamena-arches-dev/main/data/lod/zenodo/reference_data_list.tsv"
+    reference_data = pd.read_csv (reference_data_list, sep = '\t')
+    l_isDescribedBy = reference_data['url'].tolist()
+
+    sickle = Sickle(site)
+    records = sickle.ListRecords(metadataPrefix=metadataPrefix, set=set)
+    # record = records.next()
+    # return record.metadata['identifier'][0]# record = records.next()
+    l = list()
+    for record in records:
+        l.append(record.metadata['identifier'][0])
+    # remove the reference data
+    l_isContinuedBy = [x for x in l if x not in l_isDescribedBy]
+    ## create the record
+    # business data
+    l_isContinuedBy_out = list()
+    for busdata in l_isContinuedBy:
+        l_isContinuedBy_out.append({'relation': 'isContinuedBy',
+                                    'identifier': busdata})
+    # reference data
+    l_isDescribedBy_out = list()   
+    for refdata in l_isDescribedBy:
+        l_isDescribedBy_out.append({'relation': 'isDescribedBy',
+                                    'identifier': refdata})
+    # merge lists
+    l_related_identifiers = l_isContinuedBy + l_isDescribedBy_out
+    return(l_related_identifiers)
