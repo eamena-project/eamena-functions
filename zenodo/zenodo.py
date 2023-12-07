63469e1e0aa28295750053e263640cd100678e1a
zoometh <thomashuet7@gmail.com>
2023-12-07 12:58:22 +0000

    basic stats

--- zenodo/zenodo.py
+++ zenodo/zenodo.py
@@ -5,7 +5,7 @@ def summed_values(data = None, fieldname = None):
     """
     Creates a dataframe summing the number of occurences for a given field
     
-    :param data: dictionary (JSON)
+    ::param data: dictionary of Heritage Places (JSON)
     """
     import pandas as pd
     from collections import Counter
@@ -21,32 +21,32 @@ def summed_values(data = None, fieldname = None):
     return df
 
 def zenodo_contributors(data = None, fieldname = "Assessment Investigator - Actor", 
-                        contributors_layout = {"name": None, "type": "DataCollector"}):
+                        contributors_layout_HP = {"name": None, "type": "DataCollector"}, contributors_layout_GS = [{'name': "University of Oxford", "type": "DataManager"},{'name': "University of Southampton", "type": "DataManager"}]):
     """
-    Creates dictionary of contributors, filling a dictionary layout (`contributors_layout`). Contributors are sorted according to the total number of their name occurences in the selected `fieldname`.
+    Creates dictionary of contributors, filling a dictionary layout (`contributors_layout_*`). Contributors are sorted according to the total number of their name occurences in the selected `fieldname`.
     
-    :param data: dictionary (JSON)
+    :param data: dictionary of Heritage Places (JSON)
     """
     if fieldname in list(data['features'][0]['properties'].keys()):
     # HPs
         df = summed_values(data, fieldname)
         CONTRIBUTORS = list()
         for name in df['name']:
-            contibut = contributors_layout.copy()
+            contibut = contributors_layout_HP.copy()
             contibut['name'] = name
             # TODO: "affiliation" and "orcid"
             contibut = {key: value for key, value in contibut.items() if value is not None and value != 'null'}
             CONTRIBUTORS.append(contibut)
     else:
     # not HPs (GS, ...)
-        CONTRIBUTORS = ''
+        CONTRIBUTORS = contributors_layout_GS
     return CONTRIBUTORS
 
 def zenodo_keywords(data = None, constant = ['EAMENA', 'MaREA', 'Cultural Heritage'], additional = None, fields = ["Country Type", "Cultural Period Type"]):
     """
     Creates a list of keywords with a constant basis (`constant`) and parsed supplementary `fields` (for space-time keywords)
     
-    :param data: dictionary (JSON)
+    :param data: dictionary of Heritage Places (JSON)
     :param additional: additional keyworks provided by the user
     """
     KEYWORDS = list()
@@ -63,7 +63,7 @@ def zenodo_dates(data = None, fields = ["Assessment Activity Date"]):
     """
     Get the min and the max of dates recorded in `fields`    
 
-    :param data: dictionary (JSON)
+    :param data: dictionary of Heritage Places (JSON)
     """
     from datetime import datetime
 
@@ -124,15 +124,46 @@ def zenodo_related_identifiers(site = 'https://zenodo.org/oai2d', set = 'user-ea
     l_related_identifiers = l_isContinuedBy + l_isDescribedBy_out
     return(l_related_identifiers)
 
+def zenodo_statistics(data = None):
+    """
+    Calculate basic statistics on HPs. Return a list with: the total number of Heritage Places; the number of Heritage Places have 1, 2, 3, ... geometries; etc.
+
+    :param data: dictionary of Heritage Places (JSON)
+    """
+    from collections import Counter
+
+    l = list()
+    LIST_HPS = list()
+    for i in range(len(data['features'])):
+        ea_id = data['features'][i]['properties']['EAMENA ID']
+        l.append(ea_id)
+    my_dict = {i:l.count(i) for i in l}
+    value_counts = Counter(my_dict.values())
+    HPS_GEOM_NB = dict(value_counts)
+    HPS_NB = sum(HPS_GEOM_NB.values())   
+    LIST_HPS.append(HPS_NB)
+    LIST_HPS.append(HPS_GEOM_NB)
+    return(LIST_HPS)
+
 # #%% test
 # import requests
 
-# GEOJSON_URL = "https://database.eamena.org/api/search/export_results?paging-filter=1&tiles=true&format=geojson&reportlink=false&precision=6&language=*&total=25&resource-type-filter=%5B%7B%22graphid%22%3A%2277d18973-7428-11ea-b4d0-02e7594ce0a0%22%2C%22name%22%3A%22Grid%20Square%22%2C%22inverted%22%3Afalse%7D%5D&map-filter=%7B%22type%22%3A%22FeatureCollection%22%2C%22features%22%3A%5B%7B%22type%22%3A%22Feature%22%2C%22properties%22%3A%7B%22id%22%3A1%2C%22buffer%22%3A%7B%22width%22%3A%220%22%2C%22unit%22%3A%22m%22%7D%2C%22inverted%22%3Afalse%7D%2C%22geometry%22%3A%7B%22type%22%3A%22MultiPolygon%22%2C%22coordinates%22%3A%5B%5B%5B%5B61.9185199386503%2C31.434624233128837%5D%2C%5B61.93140337423312%2C31.176955521472394%5D%2C%5B62.17296779141103%2C31.164072085889572%5D%2C%5B62.195513803680974%2C30.829102760736195%5D%2C%5B61.91207822085889%2C30.829102760736195%5D%2C%5B61.92496165644171%2C30.587538343558283%5D%2C%5B61.396740797546%2C30.593980061349694%5D%2C%5B61.40318251533741%2C30.316986196319018%5D%2C%5B60.797661042944775%2C30.320207055214723%5D%2C%5B60.810544478527596%2C30.68094325153374%5D%2C%5B61.07465490797545%2C30.68094325153374%5D%2C%5B61.06821319018404%2C31.083550613496932%5D%2C%5B60.82342791411042%2C31.089992331288343%5D%2C%5B60.80732361963189%2C31.312231595092026%5D%2C%5B60.54643404907974%2C31.318673312883437%5D%2C%5B60.53355061349692%2C31.437845092024542%5D%2C%5B61.9185199386503%2C31.434624233128837%5D%5D%5D%5D%7D%7D%5D%7D"
+# GEOJSON_URL = "https://database.eamena.org/api/search/export_results?paging-filter=1&tiles=true&format=geojson&reportlink=false&precision=6&language=*&total=1641&resource-type-filter=%5B%7B%22graphid%22%3A%2234cfe98e-c2c0-11ea-9026-02e7594ce0a0%22%2C%22name%22%3A%22Heritage%20Place%22%2C%22inverted%22%3Afalse%7D%5D&map-filter=%7B%22type%22%3A%22FeatureCollection%22%2C%22features%22%3A%5B%7B%22type%22%3A%22Feature%22%2C%22properties%22%3A%7B%22Grid%20ID%22%3A%22E60N30-24%22%2C%22buffer%22%3A%7B%22width%22%3A%220%22%2C%22unit%22%3A%22m%22%7D%2C%22inverted%22%3Afalse%7D%2C%22geometry%22%3A%7B%22type%22%3A%22Polygon%22%2C%22coordinates%22%3A%5B%5B%5B60.5%2C31.25%5D%2C%5B60.5%2C31.5%5D%2C%5B60.75%2C31.5%5D%2C%5B61%2C31.5%5D%2C%5B61.25%2C31.5%5D%2C%5B61.5%2C31.5%5D%2C%5B61.75%2C31.5%5D%2C%5B62%2C31.5%5D%2C%5B62%2C31.25%5D%2C%5B62.25%2C31.25%5D%2C%5B62.25%2C31%5D%2C%5B62.25%2C30.75%5D%2C%5B62%2C30.75%5D%2C%5B62%2C30.5%5D%2C%5B61.75%2C30.5%5D%2C%5B61.5%2C30.5%5D%2C%5B61.5%2C30.25%5D%2C%5B61.25%2C30.25%5D%2C%5B61%2C30.25%5D%2C%5B60.75%2C30.25%5D%2C%5B60.75%2C30.5%5D%2C%5B60.75%2C30.75%5D%2C%5B61%2C30.75%5D%2C%5B61%2C31%5D%2C%5B60.75%2C31%5D%2C%5B60.75%2C31.25%5D%2C%5B60.5%2C31.25%5D%5D%5D%7D%7D%5D%7D"
 # resp = requests.get(GEOJSON_URL)
 # data = resp.json()
+
+# #%% test
+
+
+
+
 # # %%
 
-# zenodo_contributors(data)
-# zenodo_keywords(data)
-# zenodo_dates(data)
+# zenodo_statistics(data)
+
+# # zenodo_contributors(data)
+# # zenodo_keywords(data)
+# # zenodo_dates(data)
+# # # %%
+
 # # %%
