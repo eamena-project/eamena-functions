ed4b489277b1d4045650c9cb0920ec99c7f5bd57
zoometh <thomashuet7@gmail.com>
2024-09-09 12:07:40 +0100

    varia

--- business_data.py
+++ business_data.py
@@ -1,6 +1,66 @@
 # bunch of functions to manage business data
 
 
+#%%
+def append_filter_data(input_file_path = "C:/Rprojects/eamena-arches-dev/dbs/database.eamena/data/bulk_data/db_data/Fazzan_BU_Append_Aug24_v5_rvTH.csv", uuid = 'ResourceID', r2r_fields = ['Assessment Investigator - Actor', 'Information Resource Used'], r2r_map_po = "C:/Rprojects/eamena-arches-dev/dbs/database.eamena/data/bulk_data/db_data/mapping_pers.csv", r2r_map_ir = "C:/Rprojects/eamena-arches-dev/dbs/database.eamena/data/bulk_data/db_data/mapping_ir.csv", verbose=True):
+  """
+  Grab a tabular file having data used to update and append different resources (ex: HP), filter the data that need to be 'pushed' though the BU append procedure, and data that go into the resource 2 resource import. These two procedures (BU append and resource-to-resource) follow different Python processes, respectively 'import_business_data_relations' and 'import_business_data ... -ow append'.
+
+  :param r2r_fields: Resource-to-Resource fields. It is assumed that values form these fields are human-readable values (not UUID), for example the 'Assessment Investigator - Actor' has the value 'Martin Sterry' not his UUID (ecdc771c-ff31-42c7-9ec9-522e6302e6f0). Human-readable values will be match to their UUID using a mapping table. The output of these r2r values will be exported into a CSV file 
+  :param uuid: The field having the UUID of the target resource (ie. values will be added to these resources)
+  :param r2r_map_po: Path to the Person/Organisation (po) mapping table. The later provides alignment between the human-readable values and their UUID
+  :param r2r_map_ir: Path to the Information Resources (ir) mapping table. The later provides alignment between the human-readable values and their UUID
+
+  """
+  import pandas as pd
+  import numpy as np
+    
+  df_unfiltered = pd.read_csv(input_file_path)
+  r2r_fields.insert(0, uuid)
+  # TODO trim lead/trail spaces
+  df_filtered_r2r = df_unfiltered[r2r_fields]
+  # TODO: all in one loop
+  if 'Assessment Investigator - Actor' in df_filtered_r2r.columns:
+    if verbose:
+         print("Resource to Resource: 'Assessment Investigator - Actor'")
+    # rm empty row
+    df_filtered_r2r_po = df_filtered_r2r.dropna(subset=['Assessment Investigator - Actor'])
+    df_map_r2r_po = pd.read_csv(r2r_map_po)
+    df_r2r_po_merged = pd.merge(df_filtered_r2r_po, df_map_r2r_po, on='Assessment Investigator - Actor', how='left')
+    nrow = len(df_r2r_po_merged)
+    df_r2r_po = pd.DataFrame({
+         'resourceinstanceidfrom': df_r2r_po_merged[uuid],
+         'resourceinstanceidto': df_r2r_po_merged['uuid_pers'],
+         'relationshiptype': np.repeat('http://www.ics.forth.gr/isl/CRMdig/L33_has_maker', nrow),
+         'datestarted': np.repeat('', nrow),
+         'dateended': np.repeat('', nrow),
+         'notes': np.repeat('', nrow)
+  })
+  if 'Information Resource Used' in df_filtered_r2r.columns:
+    if verbose:
+         print("Resource to Resource: 'Information Resource Used'")
+    # rm empty row
+    df_filtered_r2r_ir = df_filtered_r2r.dropna(subset=['Information Resource Used'])
+    df_map_r2r_ir = pd.read_csv(r2r_map_ir)
+    df_r2r_ir_merged = pd.merge(df_filtered_r2r_ir, df_map_r2r_ir, on='Information Resource Used', how='left')
+    nrow = len(df_r2r_ir_merged)
+    df_r2r_ir = pd.DataFrame({
+         'resourceinstanceidfrom': df_r2r_ir_merged[uuid],
+         'resourceinstanceidto': df_r2r_ir_merged['uuid_ir'],
+         'relationshiptype': np.repeat('http://www.cidoc-crm.org/cidoc-crm/P129i_is_subject_of', nrow),
+         'datestarted': np.repeat('', nrow),
+         'dateended': np.repeat('', nrow),
+         'notes': np.repeat('', nrow)
+  })
+
+  return df_r2r_po, df_r2r_ir
+
+df_r2r_po, df_r2r_ir = append_filter_data()
+# df.head()
+df_r2r_po.to_csv("C:/Rprojects/eamena-arches-dev/dbs/database.eamena/data/bulk_data/db_data/Fazzan_r2r_po.csv", index=False)
+df_r2r_ir.to_csv("C:/Rprojects/eamena-arches-dev/dbs/database.eamena/data/bulk_data/db_data/Fazzan_r2r_ir.csv", index=False)
+
+#%%
 def db_query(GEOJSON_URL = None):
 	"""
 	Return a JSON file (GeoJSON) from a GeoJSON URL
